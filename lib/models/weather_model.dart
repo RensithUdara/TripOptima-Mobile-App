c199d9f8b1c190299747f45af13b473b220553d2
RensithUdara <rensithudaragonalagoda@gmail.com>
2025-09-08 17:56:06 +0530

    Refactor WeatherModel to replace location with locationId and reorganize attributes

--- lib/models/weather_model.dart
+++ lib/models/weather_model.dart
@@ -14,129 +14,120 @@ class WeatherModel {
   final double windSpeed;
   final double windDirection;
   final double humidity;
-  final double rain;
-  final double snow;
+  final double pressure;
   final double visibility;
   final double clouds;
-  final double pressure;
+  final double rain;
+  final double snow;
 
   WeatherModel({
     required this.id,
-    required this.location,
+    required this.locationId,
     required this.timestamp,
     required this.condition,
+    required this.description,
+    required this.icon,
     required this.temperature,
     required this.feelsLike,
+    required this.minTemp,
+    required this.maxTemp,
     required this.windSpeed,
     required this.windDirection,
     required this.humidity,
-    required this.precipitation,
-    required this.visibility,
-    required this.uvIndex,
     required this.pressure,
-    required this.cloudCover,
-    required this.sunrise,
-    required this.sunset,
-    this.alerts = const {},
-    this.hourlyForecast = const {},
-    this.dailyForecast = const {},
+    required this.visibility,
+    required this.clouds,
+    required this.rain,
+    required this.snow,
   });
 
   factory WeatherModel.fromJson(Map<String, dynamic> json) {
     return WeatherModel(
       id: json['id'] ?? '',
-      location: LocationModel.fromJson(json['location'] ?? {}),
+      locationId: json['locationId'] ?? '',
       timestamp: json['timestamp'] != null
           ? DateTime.parse(json['timestamp'])
           : DateTime.now(),
       condition: json['condition'] ?? 'Unknown',
+      description: json['description'] ?? 'No description available',
+      icon: json['icon'] ?? '',
       temperature: json['temperature']?.toDouble() ?? 0.0,
       feelsLike: json['feelsLike']?.toDouble() ?? 0.0,
+      minTemp: json['minTemp']?.toDouble() ?? 0.0,
+      maxTemp: json['maxTemp']?.toDouble() ?? 0.0,
       windSpeed: json['windSpeed']?.toDouble() ?? 0.0,
-      windDirection: json['windDirection'] ?? 0,
+      windDirection: json['windDirection']?.toDouble() ?? 0.0,
       humidity: json['humidity']?.toDouble() ?? 0.0,
-      precipitation: json['precipitation']?.toDouble() ?? 0.0,
-      visibility: json['visibility']?.toDouble() ?? 0.0,
-      uvIndex: json['uvIndex']?.toDouble() ?? 0.0,
       pressure: json['pressure']?.toDouble() ?? 0.0,
-      cloudCover: json['cloudCover'] ?? 0,
-      sunrise: json['sunrise'] != null
-          ? DateTime.parse(json['sunrise'])
-          : DateTime.now(),
-      sunset: json['sunset'] != null
-          ? DateTime.parse(json['sunset'])
-          : DateTime.now(),
-      alerts: json['alerts'] ?? {},
-      hourlyForecast: json['hourlyForecast'] ?? {},
-      dailyForecast: json['dailyForecast'] ?? {},
+      visibility: json['visibility']?.toDouble() ?? 0.0,
+      clouds: json['clouds']?.toDouble() ?? 0.0,
+      rain: json['rain']?.toDouble() ?? 0.0,
+      snow: json['snow']?.toDouble() ?? 0.0,
     );
   }
 
   Map<String, dynamic> toJson() {
     return {
       'id': id,
-      'location': location.toJson(),
+      'locationId': locationId,
       'timestamp': timestamp.toIso8601String(),
       'condition': condition,
+      'description': description,
+      'icon': icon,
       'temperature': temperature,
       'feelsLike': feelsLike,
+      'minTemp': minTemp,
+      'maxTemp': maxTemp,
       'windSpeed': windSpeed,
       'windDirection': windDirection,
       'humidity': humidity,
-      'precipitation': precipitation,
-      'visibility': visibility,
-      'uvIndex': uvIndex,
       'pressure': pressure,
-      'cloudCover': cloudCover,
-      'sunrise': sunrise.toIso8601String(),
-      'sunset': sunset.toIso8601String(),
-      'alerts': alerts,
-      'hourlyForecast': hourlyForecast,
-      'dailyForecast': dailyForecast,
+      'visibility': visibility,
+      'clouds': clouds,
+      'rain': rain,
+      'snow': snow,
     };
   }
 
   WeatherModel copyWith({
     String? id,
-    LocationModel? location,
+    String? locationId,
     DateTime? timestamp,
     String? condition,
+    String? description,
+    String? icon,
     double? temperature,
     double? feelsLike,
+    double? minTemp,
+    double? maxTemp,
     double? windSpeed,
-    int? windDirection,
+    double? windDirection,
     double? humidity,
-    double? precipitation,
-    double? visibility,
-    double? uvIndex,
     double? pressure,
-    int? cloudCover,
-    DateTime? sunrise,
-    DateTime? sunset,
-    Map<String, dynamic>? alerts,
-    Map<String, dynamic>? hourlyForecast,
-    Map<String, dynamic>? dailyForecast,
+    double? visibility,
+    double? clouds,
+    double? rain,
+    double? snow,
   }) {
     return WeatherModel(
       id: id ?? this.id,
-      location: location ?? this.location,
+      locationId: locationId ?? this.locationId,
       timestamp: timestamp ?? this.timestamp,
       condition: condition ?? this.condition,
+      description: description ?? this.description,
+      icon: icon ?? this.icon,
       temperature: temperature ?? this.temperature,
       feelsLike: feelsLike ?? this.feelsLike,
+      minTemp: minTemp ?? this.minTemp,
+      maxTemp: maxTemp ?? this.maxTemp,
       windSpeed: windSpeed ?? this.windSpeed,
       windDirection: windDirection ?? this.windDirection,
       humidity: humidity ?? this.humidity,
-      precipitation: precipitation ?? this.precipitation,
-      visibility: visibility ?? this.visibility,
-      uvIndex: uvIndex ?? this.uvIndex,
       pressure: pressure ?? this.pressure,
-      cloudCover: cloudCover ?? this.cloudCover,
-      sunrise: sunrise ?? this.sunrise,
-      sunset: sunset ?? this.sunset,
-      alerts: alerts ?? this.alerts,
-      hourlyForecast: hourlyForecast ?? this.hourlyForecast,
-      dailyForecast: dailyForecast ?? this.dailyForecast,
+      visibility: visibility ?? this.visibility,
+      clouds: clouds ?? this.clouds,
+      rain: rain ?? this.rain,
+      snow: snow ?? this.snow,
     );
   }
 }
